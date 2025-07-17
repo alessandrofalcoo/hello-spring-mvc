98d812f50734ecf049fe1a0f82eb6802e90a140a
alessandrofalcoo <alefalco.1003@gmail.com>
2025-07-17 14:51:24 +0200

    Implement HomeController with basic Spring MVC annotations

--- src/main/java/org/lessons/java/spring/spring_intro/controller/HomeController.java
+++ src/main/java/org/lessons/java/spring/spring_intro/controller/HomeController.java
@@ -1,5 +1,10 @@
 package org.lessons.java.spring.spring_intro.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
 }
